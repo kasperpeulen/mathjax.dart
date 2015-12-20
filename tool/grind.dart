ae348630b9e6e50ef844ebb7c88d7e626db83d71
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-20 01:04:48 +0100

    chore: try to get firefox,content-shell on travis working

--- tool/grind.dart
+++ tool/grind.dart
@@ -23,7 +23,7 @@ void analyze() {
 
 @Task()
 void test() {
-  final platforms = ['dartium,firefox,content-shell'];
+  final platforms = ['firefox,content-shell'];
   new TestRunner().test(platformSelector: platforms);
 }
 
